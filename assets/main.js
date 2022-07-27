e5c24251a4764b48ce92fecb8ebfc86344d640b3
unknown <afiqfaridm@gmail.com>
2022-07-27 16:08:07 +0800

    second commit

--- assets/main.js
+++ assets/main.js
@@ -13,15 +13,33 @@ annual.addEventListener('keyup', () => {
 });
 
 monthly.addEventListener('keyup', () => {
-    annual.value = monthly.value * 12;
+    annual.value = Math.round(monthly.value * 12);
     weekly.value = Math.round((monthly.value * 12) / 52);
     daily.value = (((monthly.value * 12) / 52) / 5).toFixed(2);
     hourly.value = (((monthly.value * 12) / 52) / hourspw.value).toFixed(2);
 });
 
 weekly.addEventListener('keyup', () => {
-    annual.value = monthly.value * 12;
-    weekly.value = Math.round((monthly.value * 12) / 52);
-    daily.value = (((monthly.value * 12) / 52) / 5).toFixed(2);
-    hourly.value = (((monthly.value * 12) / 52) / hourspw.value).toFixed(2);
+    annual.value = Math.round(weekly.value * 52);
+    monthly.value = Math.round((weekly.value * 52) / 12);
+    daily.value = (weekly.value / 5).toFixed(2);
+    hourly.value = (weekly.value / hourspw.value).toFixed(2);
+});
+
+daily.addEventListener('keyup', () => {
+    annual.value = Math.round((daily.value * 5) * 52);
+    monthly.value = Math.round(((daily.value * 5) * 52) / 12);
+    weekly.value = Math.round(daily.value * 5);
+    hourly.value = (daily.value / 8).toFixed(2);
+});
+
+hourly.addEventListener('keyup', () => {
+    annual.value = Math.round((hourly.value * hourspw.value) * 52);
+    monthly.value = Math.round(((hourly.value * hourspw.value) * 52) / 12);
+    weekly.value = Math.round(hourly.value * hourspw.value);
+    daily.value = ((hourly.value * hourspw.value) / 5).toFixed(2);
+});
+
+hourspw.addEventListener('keyup', () => {
+    hourly.value = (weekly.value / hourspw.value).toFixed(2);
 });
